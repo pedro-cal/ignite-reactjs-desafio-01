d7ff5b1b7c5aa266efa70cfd8751906c6e7a3203
pedro-cal <pedroluna2@hotmail.com>
2021-08-06 11:01:51 -0300

    Completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+// import { v4 as uuid } from 'uuid'
 
 import '../styles/tasklist.scss'
 
@@ -16,14 +17,34 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+    
+    const newTask:Task = {
+      // id: uuid(),
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+    
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasks = tasks.map(t => {
+      if (t.id === id) {
+        t.isComplete = !t.isComplete;
+      }
+      return t;
+    });
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const updatedTasks = tasks.filter(t => t.id !== id);
+    setTasks(updatedTasks);
   }
 
   return (
